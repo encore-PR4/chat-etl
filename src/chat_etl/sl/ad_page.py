36ebc7e5c03b1e06106b819cabd027712c83b97c
lsiwh37249 <lsiwh37249@gmail.com>
2024-10-27 21:43:29 +0900

    ad_page.py

--- src/chat_etl/sl/ad_page.py
+++ src/chat_etl/sl/ad_page.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import json
 import streamlit as st
+import time
 
+@st.cache_data
 def load_data():
     # file 읽어오기
     # parquet 파일을 json으로 변경
-    d = pd.read_parquet("/home/kim1/data/tmp")
+    d = pd.read_parquet("/home/ubuntu/apps/streamlit/data")
     d_json = d.to_json(orient='records')  # JSON 변환
     return json.loads(d_json)  # JSON 포맷으로 변환하여 반
     
@@ -53,7 +55,41 @@ def show_table(user, unique_id):
     else:
         st.write("선택한 조건에 맞는 데이터가 없습니다.")
 
-# Streamlit 애플리케이션 실행
+def show_bar(x='specific_path'):
+    data = load_data()
+    df = pd.DataFrame(data)
+
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    grouped_df = df.groupby(x).size().reset_index(name='count')
+    grouped_df = grouped_df[[x, 'count']]
+    # Streamlit에서 그룹화된 데이터프레임을 시각화
+    st.write("Grouped Data by 'id':")
+    st.dataframe(grouped_df)
+
+    # 막대그래프 그리기
+    fig, ax = plt.subplots()
+    sns.barplot(x=grouped_df['specific_path'], y=grouped_df['count'])
+    ax.set_xlabel('id')
+    ax.set_ylabel('request count')
+    ax.set_title('ID BY REQUEST COUNT')
+
+    # Streamlit에 그래프 표시
+    st.pyplot(fig)
+
+def delete_cache():
+    # 캐시 주기적 삭제 설정
+    CACHE_RESET_INTERVAL = 30  # 30초마다 캐시 지우기
+    if 'last_cache_clear_time' not in st.session_state:
+        st.session_state.last_cache_clear_time = time.time()
+
+    current_time = time.time()
+    if current_time - st.session_state.last_cache_clear_time >= CACHE_RESET_INTERVAL:
+        st.cache_data.clear()  # 모든 캐시 지우기
+        st.session_state.last_cache_clear_time = current_time
+        st.write("캐시가 지워졌습니다.")
+        print("캐시 삭제")
+
 try:
     if __name__ == "__main__":
         # 고유한 키 식별자를 사용하여 사용자와 num을 선택
@@ -62,6 +98,9 @@ try:
 
         # 선택한 값에 맞는 테이블을 보여줌
         show_table(selected_user, "table_1")
+        show_bar()
+        
+        delete_cache()
 
 except ConnectionError as e:
     st.error("서버가 불안정하여 DB에 연결할 수 없습니다. 나중에 다시 시도해주세요.")
--- src/chat_etl/spark_stream.py
+++ src/chat_etl/spark_stream.py
@@ -1,23 +1,31 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, regexp_replace, expr
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
+from urllib.parse import unquote
+import os
+import sys
+ds_nodash = sys.argv[1]
+
+print("========"*100)
+print(ds_nodash)
 
 spark = (
-    SparkSession 
-    .builder 
-    .appName("kafka_streaming") 
-    .master("local[*]") 
+    SparkSession
+    .builder
+    .appName("kafka_streaming")
+    .master("local[*]")
+    .config("spark.driver.memory", "1g")
     .getOrCreate()
-	)
+        )
 
 
-topic_name = "chat"
+topic_name = "message-topic"
 #bootstrap_server = "3.36.52.201:9092"
 bootstrap_server = "localhost:9092"
-#group_id = "chat-group"  
+#group_id = "chat-group"
 
 df = spark \
-  .read \
+  .readStream \
   .format("kafka") \
   .option("kafka.bootstrap.servers", bootstrap_server) \
   .option("subscribe", topic_name) \
@@ -44,6 +52,31 @@ json_schema = StructType([
 df = df \
     .withColumn("json_data", from_json(col("value").cast("string"), json_schema)) \
     .selectExpr("json_data.*")  # JSON 필드를 개별 컬럼으로 추출
-    
+
 print(df.count())
 df.show()
+
+# 임시로 response 컬럼 추가
+df = df.withColumn("response", expr("'tmp'"))  # 모든 행에 'tmp' 값을 추가
+
+
+# 공통 접두사 구하는 함수
+def find_common_prefix(paths):
+    return os.path.commonprefix(paths)
+
+# 공통 접두사 추출
+common_part = find_common_prefix(df.select("path").rdd.flatMap(lambda x: x).collect())
+
+# 공통된 접두사를 새로운 컬럼 'base_path'로 저장하고, 나머지 부분을 'specific_path'로 분리
+df = df.withColumn("base_path", expr(f"'{common_part}'")) \
+       .withColumn("specific_path", regexp_replace(col("path"), common_part, ""))
+
+#df = df.drop("path")
+print(df.count())
+# DataFrame 내용 출력
+df.show(truncate=False)
+
+# Parquet 파일로 저장, 'specific_path'를 파티션 컬럼으로 설정
+save_dir = f"/home/ubuntu/data/tmp/{ds_nodash}/{common_part}"  # 저장할 디렉토리 경로
+
+df.write.partitionBy("specific_path").parquet(save_dir, mode="overwrite")  # 'overwrite' 모드로 저장
