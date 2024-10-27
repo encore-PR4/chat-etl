4a0bed8861f77996738d11276c716dab9d294279
lsiwh37249 <lsiwh37249@gmail.com>
2024-10-28 08:56:47 +0900

    ad_page.py

--- src/chat_etl/sl/ad_page.py
+++ src/chat_etl/sl/ad_page.py
@@ -2,12 +2,19 @@ import pandas as pd
 import json
 import streamlit as st
 import time
+import datetime
 
-@st.cache_data
+# 전역 변수 초기화
+date = None
+
+@st.cache_data(ttl=120)
 def load_data():
+    global date
     # file 읽어오기
-    # parquet 파일을 json으로 변경
-    d = pd.read_parquet("/home/ubuntu/apps/streamlit/data")
+    
+    # date를 문자열로 변환하고 "-"를 제거
+    date = date.isoformat().replace("-", "")
+    d = pd.read_parquet(f"/home/ubuntu/data/tmp/{date}")
     d_json = d.to_json(orient='records')  # JSON 변환
     return json.loads(d_json)  # JSON 포맷으로 변환하여 반
     
@@ -15,14 +22,16 @@ def load_data():
 def select_request_user(unique_id):
     data = load_data()
     df = pd.DataFrame(data)
-    options = ["모든 사용자"] + df['specific_path'].unique().tolist()
+    print("===="*30)
+    print(df) 
+    options = ["모든 사용자"] + df['request_senderId'].unique().tolist()
 
     # 사용자 선택
     user = st.selectbox(
-        "request user 선택",
+        "request_senderId 선택",
         options,
         index=0,  # 기본값은 "모든 사용자"
-        placeholder="request_user를 선택해주세요.",
+        placeholder="request_senderId를 선택해주세요.",
         key=f"select_request_user_{unique_id}"  # 고유한 key 값 설정
     )
 
@@ -34,10 +43,10 @@ def select_num(user, unique_id):
 
     # 선택된 user에 따라 num 값을 필터링
     if user != "모든 사용자":
-        filtered_df = df[df['specific_path'] == user]
-        options = ["모든 번호"] + filtered_df['specific_path'].unique().tolist()
+        filtered_df = df[df['request_senderId'] == user]
+        options = ["모든 번호"] + filtered_df['request_senderId'].unique().tolist()
     else:
-        options = ["모든 번호"] + df['specific_path'].unique().tolist()  # 전체 번호 표시
+        options = ["모든 번호"] + df['request_senderId'].unique().tolist()  # 전체 번호 표시
 
     return options
 
@@ -47,7 +56,7 @@ def show_table(user, unique_id):
 
     # 사용자가 선택한 user와 num에 따라 데이터 필터링
     if user != "모든 사용자":
-        df = df[df['specific_path'] == user]
+        df = df[df['request_senderId'] == user]
 
     # 필터링된 테이블 출력
     if not df.empty:
@@ -55,7 +64,7 @@ def show_table(user, unique_id):
     else:
         st.write("선택한 조건에 맞는 데이터가 없습니다.")
 
-def show_bar(x='specific_path'):
+def show_bar(x='request_senderId'):
     data = load_data()
     df = pd.DataFrame(data)
 
@@ -69,38 +78,27 @@ def show_bar(x='specific_path'):
 
     # 막대그래프 그리기
     fig, ax = plt.subplots()
-    sns.barplot(x=grouped_df['specific_path'], y=grouped_df['count'])
+    sns.barplot(x=grouped_df['request_senderId'], y=grouped_df['count'])
     ax.set_xlabel('id')
-    ax.set_ylabel('request count')
+    ax.set_ylabel('request_senderId')
     ax.set_title('ID BY REQUEST COUNT')
 
     # Streamlit에 그래프 표시
     st.pyplot(fig)
 
-def delete_cache():
-    # 캐시 주기적 삭제 설정
-    CACHE_RESET_INTERVAL = 30  # 30초마다 캐시 지우기
-    if 'last_cache_clear_time' not in st.session_state:
-        st.session_state.last_cache_clear_time = time.time()
+def main():
+    global date  # 전역 변수를 사용하겠다고 선언
 
-    current_time = time.time()
-    if current_time - st.session_state.last_cache_clear_time >= CACHE_RESET_INTERVAL:
-        st.cache_data.clear()  # 모든 캐시 지우기
-        st.session_state.last_cache_clear_time = current_time
-        st.write("캐시가 지워졌습니다.")
-        print("캐시 삭제")
+    date = st.date_input("Date", value=datetime.date(2024, 1, 1))  # 전역 변수에 값 할당
+    
+    # 고유한 키 식별자를 사용하여 사용자와 num을 선택
+    selected_user = select_request_user("user_table_1")
+    selected_num = select_num(selected_user, "num_table_1")
 
-try:
-    if __name__ == "__main__":
-        # 고유한 키 식별자를 사용하여 사용자와 num을 선택
-        selected_user = select_request_user("user_table_1")
-        selected_num = select_num(selected_user, "num_table_1")
+    # 선택한 값에 맞는 테이블을 보여줌
+    show_table(selected_user, "table_1")
+    show_bar()
 
-        # 선택한 값에 맞는 테이블을 보여줌
-        show_table(selected_user, "table_1")
-        show_bar()
-        
-        delete_cache()
-
-except ConnectionError as e:
-    st.error("서버가 불안정하여 DB에 연결할 수 없습니다. 나중에 다시 시도해주세요.")
+# Streamlit 애플리케이션 실행
+if __name__ == "__main__":
+    main()
